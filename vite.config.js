b6d9e60c891d96f16703e7d14ec727a37e9ef775
ishan-meridian-17 <ishan.gupta@onmeridian.com>
2025-05-20 13:05:30 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,38 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from 'tailwindcss'
 
-const server = 'https://challenge-based-face-recognition-hnc5a2axefaehmfj.eastus-01.azurewebsites.net'
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: './',  // ✅ REQUIRED FOR VERCEL TO LOAD ASSETS CORRECTLY
   server: {
     proxy: {
       '/api': {
-        target: server,
+        target: 'https://challenge-based-face-recognition-hnc5a2axefaehmfj.eastus-01.azurewebsites.net',
         changeOrigin: true,
-        // secure: false, // 👈 ignore SSL errors (only for development)
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
     }
   }
 })
-
-
-// import {defineConfig} from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from "tailwindcss";
-
-// const server = 'http://localhost:5000';
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react(), tailwindcss()],
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: server,
-//         rewrite: (path) => path.replace(/^\/api/, ''),
-//       }
-//     },
-//   },
-// })
